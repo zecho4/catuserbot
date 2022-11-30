403183e4eb9a57acaf89adbf6097f9454b6cd39a
Zecho satria <61011386+zecho4@users.noreply.github.com>
2022-11-30 23:08:30 +0700

    Update exampleconfig.py
    Updet

--- exampleconfig.py
+++ exampleconfig.py
@@ -10,7 +10,7 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://hcqkqjrn:iNfxe5aFIXzJTLqpfU9iWXgdj7OzHSyB@hansken.db.elephantsql.com/hcqkqjrn"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOKUBu5cnrBRYqE8RK_E70uftrXKGjcGFqxwuXQFoGZRxAwQRCQB-VXnvWmn4ad3IsdTtyeEpz-RYcCgudvy5eSxLf_G3doejEgfKTwZqCuXZNabPa10CbpuoVYsvO2TAb-4tNefkJ7fGobmgH3qMsC-vx9BGI-N89aBmvPIngL2vN_ucVlrv2MWME7eyJyTbTGRQjVDFdQuQmuePhE220tmRkT11jD02hLuAOe55E5eEOLtcBwaMIeCSlMkesHL9gnBc0CroM-Mj5_E1ezQZaxfNjxIisftoiafdHh74TS3dvBQBGBwKqMicDVTWFm5j677RjtM2_KeQjvDzIUvUVBi11Jc="
+    STRING_SESSION = "1BVtsOMEBuxPyA3_rMbEc-HWQyfMeXLsFhZbZ0CG70_Ssc8j0bEyolAK_GwAHcBQfy22o9NglDgvdSsNXamdL2eJ1FT5iLaljhl7WOzyOrqG2fwoSldIUCz1WUnSVOs-GEV-yDKwn7kzlyG8FppVpuRUDNBfpC321f_thlT-uYcZQtZIJy2vDXbmWs_ytJVJQ2aVu1wr-nt1lnh1Ff_ogrVQYeDq2IlNZYWbY5pNfVkDNmKXfgOQgHNIBBOgwll7DhmJHQcE-htMTGl2oB3h6ZZErW_Z-_Fg5COn2dZu9kX-tyhlGJjBUbqagGp30FkngtpkncL2nZpR1bYV0C-OAA_cu1SYWM="
     # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "5876358843:AAFBwdxL3Ev11aqLcQR8_xkvOTyrmDWStY0"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
