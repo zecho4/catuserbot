11e4d27cfad63a48a0b8bceb3b83ba1cad5333f8
Zecho satria <61011386+zecho4@users.noreply.github.com>
2022-11-30 23:22:24 +0700

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -12,7 +12,7 @@ class Development(Config):
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
     STRING_SESSION = "1BVtsOMEBuxPyA3_rMbEc-HWQyfMeXLsFhZbZ0CG70_Ssc8j0bEyolAK_GwAHcBQfy22o9NglDgvdSsNXamdL2eJ1FT5iLaljhl7WOzyOrqG2fwoSldIUCz1WUnSVOs-GEV-yDKwn7kzlyG8FppVpuRUDNBfpC321f_thlT-uYcZQtZIJy2vDXbmWs_ytJVJQ2aVu1wr-nt1lnh1Ff_ogrVQYeDq2IlNZYWbY5pNfVkDNmKXfgOQgHNIBBOgwll7DhmJHQcE-htMTGl2oB3h6ZZErW_Z-_Fg5COn2dZu9kX-tyhlGJjBUbqagGp30FkngtpkncL2nZpR1bYV0C-OAA_cu1SYWM="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "5876358843:AAFBwdxL3Ev11aqLcQR8_xkvOTyrmDWStY0"
+    TG_BOT_TOKEN = "932256464:AAFEo9zUefj2MkgaugLyKXhWhDzZDKko0Bc"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -1001841648449
     # command handler
